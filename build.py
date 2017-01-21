074f99cfa163cf58d970390c284b015842514ee0
Sixten Hilborn <hilborn90@gmail.com>
2017-01-21 15:27:47 +0100

    Fix RapidJSON dependency in CI

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from conan.packager import ConanMultiPackager
 import platform
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(args="--build RapidJSON")
     builder.add_common_builds(shared_option_name="OGRE:shared", pure_c=False)
     builder.run()
 
