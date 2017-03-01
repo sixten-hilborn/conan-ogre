306c6108bc31a7219963d135ed0943f8f5599b06
Sixten Hilborn <hilborn90@gmail.com>
2017-03-01 21:22:18 +0100

    Disable VS2010 in CI

--- build.py
+++ build.py
@@ -4,5 +4,11 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build RapidJSON")
     builder.add_common_builds(shared_option_name="OGRE:shared", pure_c=False)
+    # Disable VS2010 because of missing DirectX stuff
+    builder.builds = [
+        [settings, options]
+        for settings, options in builder.builds
+        if not (settings["compiler"] == "Visual Studio" and settings["compiler.version"] == "10")
+    ]
     builder.run()
 
