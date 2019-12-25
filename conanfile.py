0b666f726d3f585f4117b7017cf8d50dbc2c37ef
Sixten Hilborn <sixten.hilborn@gmail.com>
2019-12-25 23:29:38 +0100

    linux fix

--- conanfile.py
+++ conanfile.py
@@ -132,8 +132,8 @@ class OgreConan(ConanFile):
         ]
         if not self.options.shared and self.settings.os == 'Windows':
             self.cpp_info.libs = [lib+'Static' for lib in self.cpp_info.libs]
-        is_apple = (self.settings.os == 'Macos' or self.settings.os == 'iOS')
-        if self.settings.build_type == "Debug" and not is_apple:
+        #is_apple = (self.settings.os == 'Macos' or self.settings.os == 'iOS')
+        if self.settings.build_type == "Debug" and self.settings.os == 'Windows': #not is_apple:
             self.cpp_info.libs = [lib+'_d' for lib in self.cpp_info.libs]
 
         if self.settings.os == 'Linux':
