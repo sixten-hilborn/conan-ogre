39aded6d12c997f714e9c34f0e1bc897e623a1c7
Sixten Hilborn <sixten.hilborn@gmail.com>
2020-06-21 23:44:21 +0200

    Add most cmake options as conan options

--- conanfile.py
+++ conanfile.py
@@ -28,26 +28,60 @@ class OgreConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
-        "with_cg": [True, False],
         "with_rendersystem_d3d9": [True, False],
         "with_rendersystem_d3d11": [True, False],
         "with_rendersystem_gl3plus": [True, False],
         "with_rendersystem_gl": [True, False],
         "with_rendersystem_gles2": [True, False],
+        "with_plugin_bsp": [True, False],
+        "with_plugin_octree": [True, False],
+        "with_plugin_pfx": [True, False],
+        "with_plugin_dot_scene": [True, False],
+        "with_plugin_pcz": [True, False],
+        "with_plugin_cg": [True, False],
+        "with_component_paging": [True, False],
+        "with_component_meshlodgenerator": [True, False],
+        "with_component_terrain": [True, False],
+        "with_component_volume": [True, False],
+        "with_component_property": [True, False],
+        "with_component_overlay": [True, False],
+        "with_component_overlay_imgui": [True, False],
+        "with_component_hlms": [True, False],
+        "with_component_bites": [True, False],
+        "with_component_python": [True, False],
+        "with_component_java": [True, False],
+        "with_component_csharp": [True, False],
+        "with_component_rtshadersystem": [True, False],
     }
     default_options = {
         "shared": True,
-        "with_cg": True,
         "freetype:shared": False,
         "with_rendersystem_gl3plus": True,
         "with_rendersystem_gl": True,
         "with_rendersystem_gles2": False,
+        "with_plugin_bsp": True,
+        "with_plugin_octree": True,
+        "with_plugin_pfx": True,
+        "with_plugin_dot_scene": True,
+        "with_plugin_pcz": True,
+        "with_plugin_cg": True,
+        "with_component_paging": True,
+        "with_component_meshlodgenerator": True,
+        "with_component_terrain": True,
+        "with_component_volume": True,
+        "with_component_property": True,
+        "with_component_overlay": True,
+        "with_component_overlay_imgui": False,  # TODO: Enable by default?
+        "with_component_hlms": False,
+        "with_component_bites": True,
+        "with_component_python": False,
+        "with_component_java": False,
+        "with_component_csharp": False,
+        "with_component_rtshadersystem": True,
     }
     exports_sources = ['patches*']
     requires = (
         "freeimage/3.18.0@sixten-hilborn/stable",
-        "freetype/2.10.1",
-        "libpng/1.6.37",  # override freetype's libpng
         "zlib/1.2.11"
     )
     url = "http://github.com/sixten-hilborn/conan-ogre"
@@ -67,12 +101,30 @@ class OgreConan(ConanFile):
                 self.options.with_rendersystem_d3d11 = False
 
     def configure(self):
-        if 'x86' not in str(self.settings.arch):
-            self.options.with_cg = False
+        if 'x86' not in str(self.settings.arch):  # Ogre's CMake file contains this condition: "Cg_FOUND;NOT APPLE_IOS;NOT WINDOWS_STORE;NOT WINDOWS_PHONE"
+            self.options.with_plugin_cg = False
 
     def requirements(self):
-        if self.options.with_cg:
+        if self.options.with_plugin_dot_scene:
+            self.requires("pugixml/1.10")
+        if self.options.with_plugin_cg:
             self.requires("Cg/3.1@sixten-hilborn/stable")
+        if self.options.with_component_overlay:
+            self.requires("freetype/2.10.1")
+            self.requires("libpng/1.6.37")  # override freetype's libpng
+        if self.options.with_component_overlay_imgui:
+            if not self.options.with_component_overlay:
+                raise ConanInvalidConfiguration("with_component_overlay_imgui requires with_component_overlay")
+            self.requires("imgui/1.73@bincrafters/stable")  # FIXME: this package does not provide header <imgui_freetype.h>
+        if self.options.with_component_bites and not self.options.with_component_overlay:
+            raise ConanInvalidConfiguration("with_component_bites requires with_component_overlay")
+        if not self.options.shared:
+            if self.options.with_component_python:
+                raise ConanInvalidConfiguration("with_component_python is only supported for shared builds")
+            if self.options.with_component_java and self.settings.os != "Android":
+                raise ConanInvalidConfiguration("with_component_java is only supported for shared builds")
+            if self.options.with_component_csharp:
+                raise ConanInvalidConfiguration("with_component_csharp is only supported for shared builds")
 
     def build_requirements(self):
         if self.settings.os == 'Linux':
@@ -144,6 +196,25 @@ class OgreConan(ConanFile):
         cmake.definitions['OGRE_BUILD_RENDERSYSTEM_GL3PLUS'] = self.options.with_rendersystem_gl3plus
         cmake.definitions['OGRE_BUILD_RENDERSYSTEM_GL'] = self.options.with_rendersystem_gl
         cmake.definitions['OGRE_BUILD_RENDERSYSTEM_GLES2'] = self.options.with_rendersystem_gles2
+        cmake.definitions['OGRE_BUILD_PLUGIN_BSP'] = self.options.with_plugin_bsp
+        cmake.definitions['OGRE_BUILD_PLUGIN_OCTREE'] = self.options.with_plugin_octree
+        cmake.definitions['OGRE_BUILD_PLUGIN_PFX'] = self.options.with_plugin_pfx
+        cmake.definitions['OGRE_BUILD_PLUGIN_DOT_SCENE'] = self.options.with_plugin_dot_scene
+        cmake.definitions['OGRE_BUILD_PLUGIN_PCZ'] = self.options.with_plugin_pcz
+        cmake.definitions['OGRE_BUILD_PLUGIN_CG'] = self.options.with_plugin_cg
+        cmake.definitions['OGRE_BUILD_COMPONENT_PAGING'] = self.options.with_component_paging
+        cmake.definitions['OGRE_BUILD_COMPONENT_MESHLODGENERATOR'] = self.options.with_component_meshlodgenerator
+        cmake.definitions['OGRE_BUILD_COMPONENT_TERRAIN'] = self.options.with_component_terrain
+        cmake.definitions['OGRE_BUILD_COMPONENT_VOLUME'] = self.options.with_component_volume
+        cmake.definitions['OGRE_BUILD_COMPONENT_PROPERTY'] = self.options.with_component_property
+        cmake.definitions['OGRE_BUILD_COMPONENT_OVERLAY'] = self.options.with_component_overlay
+        cmake.definitions['OGRE_BUILD_COMPONENT_OVERLAY_IMGUI'] = self.options.with_component_overlay_imgui
+        cmake.definitions['OGRE_BUILD_COMPONENT_HLMS'] = self.options.with_component_hlms
+        cmake.definitions['OGRE_BUILD_COMPONENT_BITES'] = self.options.with_component_bites
+        cmake.definitions['OGRE_BUILD_COMPONENT_PYTHON'] = self.options.with_component_python
+        cmake.definitions['OGRE_BUILD_COMPONENT_JAVA'] = self.options.with_component_java
+        cmake.definitions['OGRE_BUILD_COMPONENT_CSHARP'] = self.options.with_component_csharp
+        cmake.definitions['OGRE_BUILD_COMPONENT_RTSHADERSYSTEM'] = self.options.with_component_rtshadersystem
         if self.settings.compiler == 'Visual Studio':
             cmake.definitions['OGRE_CONFIG_STATIC_LINK_CRT'] = str(self.settings.compiler.runtime).startswith('MT')
         cmake.configure(build_folder='_build', source_folder=self.folder)
@@ -184,26 +255,39 @@ class OgreConan(ConanFile):
             if self._with_opengl():
                 self.cpp_info.libs.append('OgreGLSupport')
 
-            if self.options.with_cg:
+            if self.options.with_plugin_cg:
                 self.cpp_info.libs.append('Plugin_CgProgramManager')
+            if self.options.with_plugin_bsp:
+                self.cpp_info.libs.append('Plugin_BSPSceneManager')
+            if self.options.with_plugin_octree:
+                self.cpp_info.libs.append('Plugin_OctreeSceneManager')
+            if self.options.with_plugin_pfx:
+                self.cpp_info.libs.append('Plugin_ParticleFX')
+            if self.options.with_plugin_pcz:
+                self.cpp_info.libs.append('Plugin_OctreeZone')
+                self.cpp_info.libs.append('Plugin_PCZSceneManager')
+            if self.options.with_plugin_dot_scene:
+                self.cpp_info.libs.append('Plugin_DotScene')
 
-            self.cpp_info.libs.extend([
-                'Plugin_BSPSceneManager',
-                'Plugin_OctreeSceneManager',
-                'Plugin_OctreeZone',
-                'Plugin_ParticleFX',
-                'Plugin_PCZSceneManager',
-            ])
-
-        self.cpp_info.libs.extend([
-            'OgreMain',
-            'OgreOverlay',
-            'OgrePaging',
-            'OgreProperty',
-            'OgreRTShaderSystem',
-            'OgreTerrain'
-        ])
-        # 'OgreBites', OgreHLMS', 'OgreMeshLodGenerator', 'OgreVolume',
+        self.cpp_info.libs.append('OgreMain')
+        if self.options.with_component_overlay:
+            self.cpp_info.libs.append('OgreOverlay')
+        if self.options.with_component_paging:
+            self.cpp_info.libs.append('OgrePaging')
+        if self.options.with_component_property:
+            self.cpp_info.libs.append('OgreProperty')
+        if self.options.with_component_rtshadersystem:
+            self.cpp_info.libs.append('OgreRTShaderSystem')
+        if self.options.with_component_terrain:
+            self.cpp_info.libs.append('OgreTerrain')
+        if self.options.with_component_meshlodgenerator:
+            self.cpp_info.libs.append('OgreMeshLodGenerator')
+        if self.options.with_component_volume:
+            self.cpp_info.libs.append('OgreVolume')
+        if self.options.with_component_bites:
+            self.cpp_info.libs.append('OgreBites')
+        if self.options.with_component_hlms:
+            self.cpp_info.libs.append('OgreHLMS')
 
 
         if not self.options.shared: #and self.settings.os == 'Windows':
